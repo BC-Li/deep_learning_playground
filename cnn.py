1c0f658282a75dd559ec8d55596f35cbfe59d074
BC-Li <251156266@qq.com>
2022-03-01 11:12:28 +0800

    [code] add cnn.py

---
+++ cnn.py
@@ -0,0 +1,366 @@
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils import data
+import numpy as np
+import time
+from torch import nn
+from IPython import display
+import matplotlib.pyplot as plt
+from torch.nn.modules import flatten
+from torch.nn.modules.conv import Conv2d
+from torch.nn.modules.pooling import AvgPool2d
+
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib.
+
+    Defined in :numref:`sec_calculus`"""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+def use_svg_display():  # @save
+    """Use the svg format to display a plot in Jupyter."""
+    display.set_matplotlib_formats("svg")
+
+
+def try_gpu(i=0):  # @save
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f"cuda:{i}")
+    return torch.device("cpu")
+
+
+class Timer:
+    """Record multiple running times."""
+
+    def __init__(self):
+        """Defined in :numref:`subsec_linear_model`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(
+        self,
+        xlabel=None,
+        ylabel=None,
+        legend=None,
+        xlim=None,
+        ylim=None,
+        xscale="linear",
+        yscale="linear",
+        fmts=("-", "m--", "g-.", "r:"),
+        nrows=1,
+        ncols=1,
+        figsize=(3.5, 2.5),
+    ):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [
+                self.axes,
+            ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        # display.display(self.fig)
+        self.fig.savefig("result.png")
+
+        display.clear_output(wait=True)
+
+
+class Accumulator:  # @save
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+astype = lambda x, *args, **kwargs: x.type(*args, **kwargs)
+argmax = lambda x, *args, **kwargs: x.argmax(*args, **kwargs)
+reduce_sum = lambda x, *args, **kwargs: x.sum(*args, **kwargs)
+
+
+def accuracy(y_hat, y):
+    """Compute the number of correct predictions.
+
+    Defined in :numref:`sec_softmax_scratch`"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = argmax(y_hat, axis=1)
+    cmp = astype(y_hat, y.dtype) == y
+    return float(reduce_sum(astype(cmp, y.dtype)))
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  # @save
+    """Download the Fashion-MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="/home/bochengli/learning", train=True, transform=trans, download=True
+    )
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="/home/bochengli/learning", train=False, transform=trans, download=True
+    )
+    return (
+        data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=4),
+        data.DataLoader(mnist_test, batch_size, shuffle=False, num_workers=4),
+    )
+
+
+# 网络部分
+
+lenet = nn.Sequential(
+    nn.Conv2d(1, 6, kernel_size=5, padding=2),
+    nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),  # 28*28->14*14
+    nn.Conv2d(6, 16, kernel_size=5),
+    nn.Sigmoid(),  # 14*14->10*10
+    nn.AvgPool2d(kernel_size=2, stride=2),  # 10*10->5*5
+    nn.Flatten(),
+    nn.Linear(16 * 5 * 5, 120),
+    nn.Sigmoid(),
+    nn.Linear(120, 84),
+    nn.Sigmoid(),
+    nn.Linear(84, 10),
+)
+
+alexnet = nn.Sequential(
+    nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(96, 256, kernel_size=5, padding=2),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(256, 384, kernel_size=3, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(384, 384, kernel_size=3, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(384, 256, kernel_size=3, padding=1),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Flatten(),
+    nn.Linear(6400, 4096),
+    nn.ReLU(),
+    nn.Dropout(),
+    nn.Linear(4096, 4096),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(4096, 10),
+)
+# network in network
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU(),
+    )
+
+
+nin_net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, strides=4, padding=0),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(96, 256, kernel_size=5, strides=1, padding=2),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(256, 384, kernel_size=3, strides=1, padding=1),
+    nn.MaxPool2d(3, stride=2),
+    nn.Dropout(0.5),
+    nin_block(384, 10, kernel_size=3, strides=1, padding=1),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    nn.Flatten(),
+)
+
+# inception-net
+
+
+class inception_block(nn.Module):
+    def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
+        super(inception_block, self).__init__(**kwargs)
+        self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # 线路4，3x3最大汇聚层后接1x1卷积层
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+    def forward(self, x):
+        p1 = torch.nn.functional.relu(self.p1_1(x))
+        p2 = torch.nn.functional.relu(self.p2_2(torch.nn.functional.relu(self.p2_1(x))))
+        p3 = torch.nn.functional.relu(self.p3_2(torch.nn.functional.relu(self.p3_1(x))))
+        p4 = torch.nn.functional.relu(self.p4_2(self.p4_1(x)))
+        # 在通道维度上连结输出
+        return torch.cat((p1, p2, p3, p4), dim=1)
+
+
+b1 = nn.Sequential(
+    nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3), nn.ReLU(), nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+)
+
+b2 = nn.Sequential(
+    nn.Conv2d(64, 64, kernel_size=1),
+    nn.ReLU(),
+    nn.Conv2d(64, 192, kernel_size=3, padding=1),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+)
+b3 = nn.Sequential(
+    inception_block(192, 64, (96, 128), (16, 32), 32),
+    inception_block(256, 128, (128, 192), (32, 96), 64),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+)
+b4 = nn.Sequential(
+    inception_block(480, 192, (96, 208), (16, 48), 64),
+    inception_block(512, 160, (112, 224), (24, 64), 64),
+    inception_block(512, 128, (128, 256), (24, 64), 64),
+    inception_block(512, 112, (144, 288), (32, 64), 64),
+    inception_block(528, 256, (160, 320), (32, 128), 128),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+)
+b5 = nn.Sequential(
+    inception_block(832, 256, (160, 320), (32, 128), 128),
+    inception_block(832, 384, (192, 384), (48, 128), 128),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    nn.Flatten(),
+)
+
+
+inception_net = nn.Sequential(b1, b2, b3, b4, b5, nn.Linear(1024, 10))
+# 训练部分
+X = torch.rand(size=(1, 1, 28, 28), dtype=torch.float32)
+
+
+batch_size = 128
+resize = 96
+train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size, resize=resize)
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):  # @save
+    """使用GPU计算模型在数据集上的精度"""
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_model(net, train_iter, test_iter, num_epochs, lr, device):
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+
+    net.apply(init_weights)
+    print("training on", device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(xlabel="epoch", xlim=[1, num_epochs], legend=["train loss", "train acc", "test acc"])
+    num_batches = len(train_iter)
+    timer = Timer()
+    for epoch in range(num_epochs):
+        metric = Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches, (train_l, train_acc, None))
+            print("\r", "Train epoch: %d. train acc = %f" % (epoch, train_acc), end="", flush=True)
+    test_acc = evaluate_accuracy_gpu(net, test_iter)
+    animator.add(epoch + 1, (None, None, test_acc))
+    print(f"loss {train_l:.3f}, train acc {train_acc:.3f}, " f"test acc {test_acc:.3f}")
+    print(f"{metric[2] * num_epochs / timer.sum():.1f} examples/sec " f"on {str(device)}")
+
+
+lr, num_epochs = 0.1, 10
+train_model(inception_net, train_iter, test_iter, num_epochs, lr, try_gpu())
